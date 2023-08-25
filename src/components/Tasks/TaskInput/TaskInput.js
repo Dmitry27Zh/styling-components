a3861307b95ae4db10e239d4635b0f8f47b2a99d
Dmitry <dmitry27zh@gmail.com>
2023-08-25 10:44:43 +0300

    Add TaskInput validity dynamic styles

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -4,22 +4,45 @@ import Button from "../../UI/Button/Button";
 import "./TaskInput.css";
 
 const TaskInput = (props) => {
-  const [inputText, setinputText] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [inputValidity, setInputValidity] = useState(true)
+
+  const checkInputValidity = (value) => {
+    return value.trim().length !== 0
+  }
 
   const taskInputChangeHandler = (event) => {
-    setinputText(event.target.value);
+    if (checkInputValidity(event.target.value)) {
+      setInputValidity(true)
+    }
+
+    setInputText(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    const currentInputValidity = checkInputValidity(inputText)
+    setInputValidity(currentInputValidity)
+
+    if (!currentInputValidity) {
+      return
+    }
+
     props.onAddTask(inputText);
   };
+  const labelStyles = {
+    color: inputValidity ? '' : 'red'
+  }
+  const inputStyles = {
+    borderColor: inputValidity ? '' : 'red',
+    backgroundColor: inputValidity ? '' : 'salmon'
+  }
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Задачи</label>
-        <input type="text" onChange={taskInputChangeHandler} />
+        <label style={labelStyles}>Задачи</label>
+        <input type="text" style={inputStyles} onChange={taskInputChangeHandler} />
       </div>
       <Button type="submit">Добавить Задачу</Button>
     </form>
