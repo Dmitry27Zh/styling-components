1439dbb4b48b84971035b1f80936fe5109519df3
Dmitry <dmitry27zh@gmail.com>
2023-08-25 21:34:12 +0300

    Use CSS Module in TaskInput

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,34 +1,6 @@
 import React, { useState } from "react";
-import styled from 'styled-components';
 import Button from "../../UI/Button/Button";
-import "./TaskInput.css";
-
-const FormControl = styled.div`
-  margin: 0.5rem 0;
-
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props) => props.invalid ? 'red' : 'black'};
-  }
-
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => props.invalid ? 'red' : '#ccc'};
-    background: ${(props) => props.invalid ? 'rgb(243, 157, 157)' : 'transparent'};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  & input:focus {
-    outline: none;
-    background: ${(props) => props.invalid ? 'rgb(243, 157, 157)' : '#c8e1e4'};
-    border-color: ${(props) => props.invalid ? 'red' : '#00358b'};
-  }
-`;
+import styles from "./TaskInput.module.css";
 
 const TaskInput = (props) => {
   const [inputText, setInputText] = useState("");
@@ -60,10 +32,10 @@ const TaskInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!inputValidity}>
+      <div className={`${styles['form-control']} ${inputValidity ? '' : styles.invalid}`}>
         <label>Задачи</label>
         <input type="text" onChange={taskInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Добавить Задачу</Button>
     </form>
   );
